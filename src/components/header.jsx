eb03cfa0ee720aecd3a8244c9744112a8e3c901e
ggome1 <bookincar@naver.com>
2024-11-22 16:01:24 +0900

    ui

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute bg-white min-h-[90px] min-w-full flex flex-col py-4 rounded-b-2xl shadow-lg whitespace-nowrap overflow-visible">
+    <div className="absolute bg-white max-h-[90px] h-[11%]  min-w-full flex flex-col py-4 rounded-b-2xl shadow-lg whitespace-nowrap overflow-visible">
       <div className="flex justify-center items-center gap-20 h-full px-8">
-        <img className="w-[200px]" src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Banner" />
+        <img className="w-[10%] min-w-[150px]" src={`${process.env.PUBLIC_URL}/images/logo.png`} alt="Banner" />
         <div className="gap-8 font-title text-[18px] md:flex hidden">
           <div className="cursor-pointer">카테고리</div>
           <div className="cursor-pointer">베스트</div>
